b954cedf68471c766c9c57b077588c9a80dbc293
pavelmp <pavelmp@gmail.com>
2016-01-13 14:52:13 -0800

    Add the function pattern for stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,28 @@
 var Stack = function() {
   var someInstance = {};
+  var length = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function() {
+    if(length === 0){
+      return "Can't pop from an empty stack";
+    }    
+    length--;
+    var result = storage[length];
+    delete storage[length];
+    return result;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
