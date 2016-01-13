c07f5cbbe0bf48578e0eb28b46fbfcc2c3f52bd1
pavelmp <pavelmp@gmail.com>
2016-01-13 15:53:50 -0800

    work for the functional-shared version of stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.length = 0;
+  _.extend(obj,stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push : function(value){
+	  this.storage[this.length] = value;	
+	  this.length++;			
+	},
+	pop : function(){
+	  if(this.length === 0){
+	  	return "Can't pop from an emtpy stack";
+	  }
+	  this.length--;
+	  var result = this.storage[this.length];
+	  delete this.storage[this.length];
+	  return result;
+	},
+	size : function(){
+	  return this.length;	
+	}
+};
 
 
