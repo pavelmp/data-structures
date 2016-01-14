d71dc615fcb7e3a260d8b0a8ef00afbdc086e17a
pavelmp <pavelmp@gmail.com>
2016-01-13 16:20:35 -0800

    add prototypal pattern version of stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var object = Object.create(stackMethods);
+  object.length = 0;
+  object.storage ={};
+  return object;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value) {
+		this.storage[this.length] = value;
+		this.length++;
+	}, 
+	pop: function () {
+		if(this.length === 0) {
+			return "can't pop off an empty stack";
+		}
+		this.length--;
+		var result = this.storage[this.length];
+		delete this.storage[this.length];
+		return result;
+	},
+	size: function () {
+		return this.length;
+	}
+};
 
 
