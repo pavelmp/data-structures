e30f8884f3d24ef1c22c9e3f8bf3de59d8c69094
pavelmp <pavelmp@gmail.com>
2016-01-13 22:22:19 -0800

    not work, but show some pseudo-logic

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,9 +1,23 @@
+// {value: 12, next :{value: 06, next: {value: 71, next: {value: 26, next: null}}}}
+//{ bana: {value: 34, next: list["apple"]}, apple:{value:56, next: list["gar"]}, gar: {value:23,next:null}, head:"bana",tail:"gar"}
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    this.tail.next = newNode;
+    this.tail = this.tail.next;
+
+    
+    this.tail = newNode;
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+
+    }
   };
 
   list.removeHead = function() {
@@ -27,3 +41,10 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+
+
+
