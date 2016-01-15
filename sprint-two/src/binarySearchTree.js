163a4de79820082ec3096fc49208c02375790904
pavelmp <pavelmp@gmail.com>
2016-01-15 12:33:24 -0800

    correctly insert values into binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,47 @@
 var BinarySearchTree = function(value) {
+	var tree = {};
+	tree.value = value;
+	tree.left = null;
+	tree.right = null;
+
+	tree.insert = function (value) {
+		 if (value < this.value) { 
+		 	if (this.left === null) {
+		 		this.left = BinarySearchTree(value);
+		 	} else {
+		 		this.left.insert(value);
+		 	}
+		 } else if (value > this.value) {
+		 	if (this.right === null) {
+		 		this.right = BinarySearchTree(value);
+		 	} else {
+		 		this.right.insert(value);
+		 	}
+		 } else {
+		 	return "already in tree, dummy";
+		 }		
+	};
+
+	tree.contains = function (value) {
+		if (this.value === value) {
+			return true;
+		} else if (this.left === null && this.right === null) {
+				return false;
+		} else {
+// we were here - finishing contains. check if left / right is null
+// if one isn't, return recursive call on that branch
+				if ()
+				return this.left.contains(value)
+
+			}
+		}
+	};
+
+	tree.depthFirstLog = function (callback) {
+
+	};
+
+	return tree;
 };
 
 
