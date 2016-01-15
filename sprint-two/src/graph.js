ab9d35ac2e533e5d9caeea9990a694e5b31e6da3
pavelmp <pavelmp@gmail.com>
2016-01-14 19:13:41 -0800

    figured out a bug, but didn't squash

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,24 +1,60 @@
+/* myGraph {0: {value: someVal, edges: [3,5]}, 
+			3: {value: someVal, edges: [5,0]}, 
+			5: {value: someVal, edges: [3,0]}}
+*/ 
 
 
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+  this.numNodes = 0;
+
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  var newNode = {};
+  newNode.value = node;
+  newNode.edges = [];
+  this[this.numNodes] = newNode;
+  this.numNodes++;
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+	for(var key in this){
+		if(this[key].value === node){
+			return true;
+		}
+	}
+	return false;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+	var nodeWithValue = null;
+	for(var key in this){
+		if(this[key].value === node){
+			nodeWithValue = key;
+			
+		}
+	}
+	
+	for(var key in this){
+		// check if it;s an object	
+		var indexOfRemovedEdge = this[key].edges.indexOf(nodeWithValue);
+		if(indexOfRemovedEdge !== -1){
+			this[key].edges.splice(indexOfRemovedEdge,1);
+		}
+	}	
+	delete this[nodeWithValue];
 };
+
+// {0: {value: 2, edges: []}}
+// {};
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
