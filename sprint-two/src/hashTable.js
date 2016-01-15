07fa849fcc1f0a7aad87730edffd35ed8a9b3dc6
pavelmp <pavelmp@gmail.com>
2016-01-15 11:15:08 -0800

    Start refactoring hash table to not store duplicated indices

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,27 +3,26 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this._indecies = {};
+
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if(this._indecies.hasOwnProperty(index) && this._indecies[index] !== k) {
-	index++;
+  if(!Array.isArray(this._storage.get(index))){
+    this._storage.set(index,[[k,v]]);
+  } else {
+    this._storage.set(this._storage.get(index).concat([k,v]));
   }
-
-  this._indecies[index] = k;
-  this._storage.set(index,v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
-  while(this._indecies[index] !== k){
-  	index++;
+  for(var x=0, arr = this._storage.get(index); x<arr.length;x++){
+    if(arr[x][0] === k){
+      return arr[x][1];      
+    }
   }
-  
-  return this._storage.get(index);
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
