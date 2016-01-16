bac86212f7d112edad757b8e58c9900396ba8cfc
pavelmp <pavelmp@gmail.com>
2016-01-16 10:58:23 -0800

    resize hashtables when needed

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,15 +1,18 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
   // initialize kv pair count var
+  this._kvCount = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   // increment kv pair count var
+  this._kvCount++;
   // if kv pair count/limit ratio > 75%
+  if(this._kvCount/this._limit > 0.75){
     // call resize(2)
+    this.resize(2);
+  }
   var index = getIndexBelowMaxForKey(k, this._limit);
   if(!Array.isArray(this._storage.get(index))){
     this._storage.set(index,[[k,v]]);
@@ -40,8 +43,12 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   // decrement kv pair count var
+  this._kvCount--;
   // if kv pair count/limit ratio < 25%
+  if(this._kvCount/this._limit < 0.25){
     // call resize (.5)
+    this.resize(0.5);
+  }  
   var index = getIndexBelowMaxForKey(k, this._limit);
   var arr = this._storage.get(index);
   for(var x=0;x<arr.length;x++){
@@ -53,16 +60,33 @@ HashTable.prototype.remove = function(k) {
 };
 
 HashTable.prototype.resize = function (multiplier) {
-  
+  //reset kv pairs:
+  this._kvCount = 0;
   // reset limit to limit * mutliplier
-  // Declare new limited array of limit
-  // traverse storage array by index
-    // at each index in storage, traverse bucket
-      // for each k,v pair, call hash function with kv pair and limit
-      // then set kv pair at new storage index
-// reassign storage to new limited array
-
-
+  this._limit *= multiplier;
+  // shallow copy old storage to a variable
+  
+  // BRUTE FORCE SOLUTION:  
+  // create normal array to hold ALL pairs
+  var allPairs = [];
+  // traverse storage
+  this._storage.each(function (bucket,i,a){
+    // if the bucket has any kv pairs
+    if(Array.isArray(bucket)){
+      // traverse each bucket
+      for(var x=0;x<bucket.length;x++){
+        //push each k,v pair to temp AllPairs array
+        allPairs.push(bucket[x]);
+      }
+    }
+  });
+  // reset this.storage to new limited array of new limit
+  this._storage = LimitedArray(this._limit);
+  //then traverse temp array 
+  for(var a = 0; a < allPairs.length; a++) {
+    //and populate our new limited array
+    this.insert(allPairs[a][0], allPairs[a][1]);
+  }  
 };
 
 
